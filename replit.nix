6df8b061dac34a1d41fbf7635e0618923d45cbad
HItmon <hitlabmodv2@gmail.com>
2025-07-12 15:49:08 +0700

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,20 @@
+{ pkgs }: {
+  deps = [
+    pkgs.nodejs-21_x
+    pkgs.ffmpeg
+    pkgs.yarn
+    pkgs.git
+    pkgs.python3
+    pkgs.pkg-config
+    pkgs.gcc
+    pkgs.make
+    pkgs.libvips
+    pkgs.libpng
+    pkgs.libjpeg
+    pkgs.zlib
+    pkgs.cairo
+    pkgs.pango
+    pkgs.glib
+    pkgs.gdk-pixbuf
+  ];
+}
